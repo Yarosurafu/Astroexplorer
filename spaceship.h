eeb8a9ede51506c612757234502580e5e30c5f75
Yarosurafu <yaroslavsvirin@gmail.com>
2021-01-16 01:58:59 +0200

    Finished spaceship-class

--- spaceship.h
+++ spaceship.h
@@ -4,17 +4,26 @@
 using namespace sf;
 
 class SpaceShip {
+	//Размеры спрайтов корабля и огней из сопел
 	const unsigned short SPRITE_SIZE_X = 49;
 	const unsigned short SPRITE_SIZE_Y = 46;
 	const unsigned short FIRE_SIZE_X = 12;
 	const unsigned short FIRE_SIZE_Y = 41;
-	const Texture m_textureArg;
-	const Texture m_fireTexture;
+
+	
+	Texture m_shipTexture;//текстура корабля
+	Texture m_fireTexture;//текстура огней
+
 	Sprite m_shipSprite; //спрайт корабля
 	Sprite m_firstFire; //спрайт огня из первого сопла
 	Sprite m_secondFire; //спрайт огня из второго сопла
+	SpaceShip& setFireSprite();//изменение положения огней относительно корабля
 
 public:
-	SpaceShip(const Texture& textureArg, const Texture& fireTexture);
-	Spaceship
+	SpaceShip();
+	SpaceShip& moveShip(int direction, float time);//смена позиции корабля
+	SpaceShip& changeFireSprite(int frame);//смена спрайта огня для анимации
+	Sprite& getFirstFireSprite() { return m_firstFire; }
+	Sprite& getSecondFireSprite() { return m_secondFire;  }
+	Sprite& getSprite() { return m_shipSprite;  }
 };
